50dcc7d59434228efde91428fbc68d2b0f0a0859
jperomingan@startechup.com <jperomingan@startechup.com>
2020-08-07 17:48:10 +0800

    organization.service.js

---
+++ services/organization.service.js
@@ -0,0 +1,34 @@
+const Organization = require('../models/organization.model')
+
+const Create = async (data) =>  {
+    try {
+        const organization = await Organization.create(data)
+        return organization
+    } catch(error) {
+        console.log('error: ', error)
+    }
+}
+
+const Find = async (data) =>  {
+    try {
+        const organizations = await Organization.find({})
+        return organizations
+    } catch(error) {
+        console.log('error: ', error)
+    }
+}
+
+const FindOne = async (query) =>  {
+    try {
+        const organization = await Organization.findOne(query)
+        return organization
+    } catch(error) {
+        console.log('error: ', error)
+    }
+}
+
+module.exports = {
+    Create,
+    Find,
+    FindOne
+}
