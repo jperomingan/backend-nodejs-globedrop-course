ed9629cfcf0ce12fad54caa71b5485143b4dd070
jperomingan@startechup.com <jperomingan@startechup.com>
2020-08-19 19:03:03 +0800

    Find and FindOne

--- services/user.service.js
+++ services/user.service.js
@@ -2,9 +2,28 @@ const User = require('../models/user.model/user.model')
 
 const Create = async (data) =>  {
     try {
-        const user = await user.create(data)
+        const user = await User.create(data)
         return user
     } catch(error) {
         console.log('error: ', error)
     }
 }
+
+const Find = async (data) =>  {
+    try {
+        const users = await User.find({})
+        return users
+    } catch(error) {
+        console.log('error: ', error)
+    }
+};
+
+const FindOne = async (query) =>  {
+    try {
+        const user = await user.findOne(query)
+        return user
+    } catch(error) {
+        console.log('error: ', error)
+    }
+};
+
