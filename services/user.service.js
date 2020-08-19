9db937aa202b428cc23b2606c6723c7ee750c659
jperomingan@startechup.com <jperomingan@startechup.com>
2020-08-19 18:52:49 +0800

    Create user

--- services/user.service.js
+++ services/user.service.js
@@ -1,1 +1,10 @@
 const User = require('../models/user.model/user.model')
+
+const Create = async (data) =>  {
+    try {
+        const user = await user.create(data)
+        return user
+    } catch(error) {
+        console.log('error: ', error)
+    }
+}
