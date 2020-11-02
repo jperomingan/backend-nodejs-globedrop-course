a22b1fbf776dad40ae39f00239ee3505f67ff476
jperomingan@startechup.com <jperomingan@startechup.com>
2020-11-02 14:06:06 +0800

    Delete files

--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const UserController = require('../controllers/user.controller')
-const router = express.Router()
-
-router.post('/user', UserController.Register);
-router.get('/users', UserController.GetAllUsersList);
-router.get('/user/:_id', UserController.GetUserById);
-
-module.exports = router;
