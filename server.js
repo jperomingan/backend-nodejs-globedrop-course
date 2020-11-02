c22af8df2b6b0d40b30df3e21802a7924d197a8a
jperomingan@startechup.com <jperomingan@startechup.com>
2020-11-02 15:06:09 +0800

    install mongoose dependencies

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Jenn');
+});
 
 app.listen(port, () => {
     console.log('Server runs at port: ', port);
