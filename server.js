88b50b8c7fd286418672f8d9c9d76919f6b9c2e3
jperomingan@startechup.com <jperomingan@startechup.com>
2020-11-03 17:04:43 +0800

    add user in server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const port = 3000;
 const { db } = require('./db/index')
 const organization = require('./routes/organization.route')
-
+const user = require('./routes/user.route')
 
 app.get('/', (req, res) => {
     res.send('Hello Jenn');
@@ -15,6 +15,7 @@ db();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({}));
 app.use(organization);
+app.use(user)
 
 app.listen(port, () => {
     console.log('Server runs at port: ', port);
