14c95f1e5cdfdd74f23d53d05490bd8d25ec62ae
jperomingan@startechup.com <jperomingan@startechup.com>
2020-11-25 15:03:49 +0800

    Add GetAdminsByOrganization in organization route

--- routes/organization.route.js
+++ routes/organization.route.js
@@ -7,6 +7,7 @@ router.get('/organization', OrganizationController.GetAllOrganizations);
 router.post('/organization', OrganizationController.AddOrganization);
 router.put('/organization/:organization_id', OrganizationController.UpdateOrganization)
 router.delete('/organization/:organization_id', OrganizationController.DeleteOrganization)
+router.get('/organization/:organization_id/admins', OrganizationController.GetAdminsByOrganization)
 
 module.exports = router;
 
