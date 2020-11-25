65305233458df36aa4fb684828aacff6523fb129
jperomingan@startechup.com <jperomingan@startechup.com>
2020-11-25 13:46:52 +0800

    Add field admin in organization

--- models/organization.model.js
+++ models/organization.model.js
@@ -5,6 +5,12 @@ const OrganizationSchema = new mongoose.Schema({
     org_country: String,
     org_city: String,
     org_picture: String,
+    admins: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user',
+        },
+    ],
     createdAt: {
         type: Date,
         default: Date.now,
